2c85122ced43ed3804dd5577e65a6491a930a319
normanmunge <normanmunge@gmail.com>
2017-02-28 18:33:47 +0300

    Added register client and agent in admin section

--- registerCustomer/models.py
+++ registerCustomer/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class registerCustomer(models.Model):
-    F_Name = models.CharField(max_length = 250),
+    F_Name = models.CharField(max_length = 250)
     L_Name = models.CharField(max_length = 250)
     Phonenumber = models.CharField(max_length = 250)
     isActive = models.CharField(max_length = 250)
     regDate = models.CharField(max_length = 250)
+
+    def __str__(self):
+        return self
